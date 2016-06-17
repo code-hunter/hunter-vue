bbcc596ef7bf97ce34ca630a02dc2d7ef4c61605
code-hunter <dylanzhou0@gmail.com>
2016-06-17 18:07:17 +0800

    add archive

--- routes/archive.js
+++ routes/archive.js
@@ -7,7 +7,23 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('archives');
+    var es = require('elasticsearch');
+    var es_client = new es.Client({host: 'localhost:9200'});
+
+    es_client.search({index : 'hound',
+                      type: 'crawler',
+                      body: {
+                          query:{
+                              match_all: {
+                              }
+                          }
+                      }
+    }).then(function (resp) {
+        var hits = resp.hits.hits;
+        res.send(hits);
+    }, function (err) {
+        console.trace(err.message)
+    })
 });
 
 module.exports = router;
