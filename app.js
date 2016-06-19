00750cb9980bff4d72388bc861424786c0f87b58
code-hunter <dylanzhou0@gmail.com>
2016-06-19 10:15:40 +0800

    add elasticsearch page

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// view jade engine
+// app.set('view engine', 'jade');
+//view ejs engin
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
